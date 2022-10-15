662af4c8d07b5e30dd99d238c3d34db8933bdb31
DarshanNalwal <darshannalwal2012@gmail.com>
2022-10-15 21:03:29 +0530

    Express framework practice till params

---
+++ LearningExpress/server.js
@@ -0,0 +1,136 @@
+const express = require("express");
+
+console.log(typeof express);
+const app = express();
+
+/**
+ * define the handler when someone makes a call
+ * 
+ * http://127.0.0.1:8000/hello
+ */
+
+app.get("/hello", (req,res) => {
+    res.status(200).send({
+        message : "Hello People!"
+    })
+});
+
+const students = {
+    1: {
+        Vishwa: {
+            name:"Vishwa",
+            age: 99,
+            subject: "Maths"
+        },
+        Mohan: {
+            name: "Mohan",
+            age: 97,
+            subject: "Science"
+        },
+        Shivani: {
+            name: "Shivani",
+            age: 97,
+            subject: "SST"
+        },
+        Jyoti: {
+            name: "Jyoti",
+            age: 97,
+            subject: "Computers"
+        }
+    },
+    2: {
+        Vishwa: {
+            name:"Vishwa",
+            age: 99,
+            subject: "Maths"
+        },
+        Shakshi: {
+            name:"Shakshi",
+            age: 10,
+            subject: "Geography"
+        },
+        Akanksha: {
+            name:"Akanksha",
+            age: 9,
+            subject: "Sanskrit"
+        }
+    }
+}
+
+/**
+ * 
+ * Code to read the Path Param
+ * 
+ * GET 127.0.0.1:8000/studentApp/classes/1/students/vishwa
+ * 
+ */
+app.get("/studentApp/classes/:classId/students/:studentName", (req,res) => {
+
+    /**
+     * Reading the path param
+     */
+
+    // req.params -> Object that contains all the path param
+
+    const classId = req.params.classId;
+    console.log(classId);
+    const studentName = req.params.studentName;
+    console.log(studentName);
+
+    return res.status(200).send({
+        studentDetails: (students[classId])[studentName]
+    });
+});
+
+/**
+ * Exercise
+ * 
+ */
+app.get("/studentApp/classes/:classId/students", (req,res) => {
+    
+    const classId = req.params.classId;
+    const studentName = req.query.studentName;
+
+    if(studentName) {
+        res.status(200).send({
+            studentDetails : (students[classId])[studentName]
+        })
+    } else {
+        res.status(200).send({
+            studentDetails : students[classId]
+        })
+    }
+});
+
+
+/**
+ * 
+ * Code to read the Query Param
+ * 127.0.0.1:8000/studentApp/classes?classId=1
+ * 
+ */
+
+app.get("/studentApp/classes", (req,res) => {
+    const classId = req.query.classId;
+
+    if(classId) {
+        res.status(200).send(students[classId]);
+    } else {
+        res.status(400).send({
+            message: "Query params was not provided"
+        })
+    }
+});
+
+
+/**
+ * 
+ * Code to read the requrest param
+ * 
+ */
+
+
+
+app.listen(8000 , () => {
+    console.log("Server started");
+});
