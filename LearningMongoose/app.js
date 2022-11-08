cd6807b119e5b6b318165d8403eae39cbb0a853b
DarshanNalwal <darshannalwal2012@gmail.com>
2022-11-08 22:55:02 +0530

    Querying using mongoose added

--- LearningMongoose/app.js
+++ LearningMongoose/app.js
@@ -22,24 +22,39 @@ async function dbOperation() {
     /**
      * Code to insert some student in the db
      */
-    try {
-        const student = await studentModel.create({
-            name: "Vishwa",
-            age: 17,
-            email: "abc123456@xyz",
-            subjects: ["Maths", "English"],
-            address: {
-                lane1: "Lane1",
-                lane2: "Lane2",
-                street: "AC-23",
-                country: "India",
-                pinCode: 560049
-            }
-        });
+    // try {
+    //     const student = await studentModel.create({
+    //         name: "Vishwa",
+    //         age: 17,
+    //         email: "abc123456@xyz",
+    //         subjects: ["Maths", "English"],
+    //         address: {
+    //             lane1: "Lane1",
+    //             lane2: "Lane2",
+    //             street: "AC-23",
+    //             country: "India",
+    //             pinCode: 560049
+    //         }
+    //     });
     
-        console.log(student);
-    } catch(e) {
-        console.log(e.message);
-    }
-    
+    //     console.log(student);
+    // } catch(e) {
+    //     console.log(e.message);
+    // }
+
+
+    /**
+     * I want to read some dcuments
+     * 
+     * With the help of id
+     */
+    const student = await studentModel.findById("636a5b3c487785cc195459fa");
+
+    console.log(student);
+
+    const students = await studentModel.find({name : "Vishwa"});
+    console.log(students);
+
+    const vishwaStudent = await studentModel.findOne({name : "Vishwa"});
+    console.log(vishwaStudent);
 }
